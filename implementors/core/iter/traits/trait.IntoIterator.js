c86339f818aa3372d666ba42b8f5483fc9016de4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2017-10-28 09:52:52 +0000

    Deploy lumol-org/soa-derive to github.com/lumol-org/soa-derive.git:gh-pages

---
+++ implementors/core/iter/traits/trait.IntoIterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["soa_derive_example"] = ["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/trait.IntoIterator.html\" title=\"trait core::iter::traits::IntoIterator\">IntoIterator</a> for <a class=\"struct\" href=\"soa_derive_example/struct.ParticleSlice.html\" title=\"struct soa_derive_example::ParticleSlice\">ParticleSlice</a>&lt;'a&gt;","impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/trait.IntoIterator.html\" title=\"trait core::iter::traits::IntoIterator\">IntoIterator</a> for &amp;'a <a class=\"struct\" href=\"soa_derive_example/struct.ParticleVec.html\" title=\"struct soa_derive_example::ParticleVec\">ParticleVec</a>","impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/trait.IntoIterator.html\" title=\"trait core::iter::traits::IntoIterator\">IntoIterator</a> for <a class=\"struct\" href=\"soa_derive_example/struct.ParticleSliceMut.html\" title=\"struct soa_derive_example::ParticleSliceMut\">ParticleSliceMut</a>&lt;'a&gt;","impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/trait.IntoIterator.html\" title=\"trait core::iter::traits::IntoIterator\">IntoIterator</a> for &amp;'a mut <a class=\"struct\" href=\"soa_derive_example/struct.ParticleVec.html\" title=\"struct soa_derive_example::ParticleVec\">ParticleVec</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
